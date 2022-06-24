fc6b884294993d8a8c3d92ef3e5cc16df0218052
Paolo ferrari <paoloferrariapple@gmail.com>
2022-06-24 17:56:22 +0200

    Created QRCode

--- WikipediaScraper/ContentView.swift
+++ WikipediaScraper/ContentView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import Foundation
+import UIKit
+import WebKit
 
 struct ContentView: View {
+    @State var risultatiScraping: [(String, Int, String)] = randomPageParsed()
+    @State var qrCode: Image = Image("")
+    @State var titolo: Text = Text("Titolo: ")
+    
     var body: some View {
-        Text("Hello, world!")
+        titolo
             .padding()
+        
+        qrCode
+            .resizable()
+            .scaledToFit()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200)
+        
+        Button("Genera Nuovo") {
+            risultatiScraping = randomPageParsed()
+            qrCode = Image(uiImage: UIImage(data: getQRCodeDate(text: "https://it.wikipedia.org/wiki/\(risultatiScraping[0].2)")!)!)
+            titolo = Text("Title: \(risultatiScraping[0].2)")
+        }
     }
 }
 
@@ -18,4 +37,48 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func randomPageParsed() -> [(String, Int, String)] {
+    let url = URL(string: "https://it.wikipedia.org/wiki/Speciale:PaginaCasuale")!
+    let html = try! String(contentsOf: url)
+    return pageParser(html)
+}
+
+func pageParser(_ htmlPage: String) -> [(String, Int, String)] {
+    var output: [(String, Int, String)] = []
+    let splitted = htmlPage.split(separator: "\n")
+    var counterTitle: Int = 0
+    var counterParagraph: Int = 0
+    
+    for i in 0..<splitted.count {
+        if splitted[i].contains("<title>") {
+            output.append(("title",
+                           counterTitle,
+                           String(splitted[i].suffix(splitted[i].count - 7).prefix(splitted[i].count - 27))))
+            counterTitle += 1
+        }
+        
+        if splitted[i].contains("<p>") {
+            output.append(("p",
+                           counterParagraph,
+                           String(splitted[i])))
+            counterParagraph += 1
+        }
+    }
+    
+    print("Output: \(output)")
+    
+    return output
+}
+
+func getQRCodeDate(text: String) -> Data? {
+    guard let filter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+    let data = text.data(using: .ascii, allowLossyConversion: false)
+    filter.setValue(data, forKey: "inputMessage")
+    guard let ciimage = filter.outputImage else { return nil }
+    let transform = CGAffineTransform(scaleX: 10, y: 10)
+    let scaledCIImage = ciimage.transformed(by: transform)
+    let uiimage = UIImage(ciImage: scaledCIImage)
+    return uiimage.pngData()!
 }
