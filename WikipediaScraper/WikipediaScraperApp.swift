5ad34a4b6f856233822a6fbaac2c6cd02b91dab8
Paolo ferrari <paoloferrariapple@gmail.com>
2022-06-24 16:04:41 +0200

    Initial Commit

---
+++ WikipediaScraper/WikipediaScraperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikipediaScraperApp.swift
+//  WikipediaScraper
+//
+//  Created by user on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WikipediaScraperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
